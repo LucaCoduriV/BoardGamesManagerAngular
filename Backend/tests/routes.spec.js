48cb5d18cbb359f6b898441f96f95edf86fc02d7
Luca <luca.coduri@cpnv.ch>
2020-02-08 20:01:07 +0100

    Création du fichier de base pour les tests

---
+++ Backend/tests/routes.spec.js
@@ -0,0 +1,33 @@
+describe("testing public routes", ()=>{
+    beforeAll((done)=>{
+        //démarrer une instance du serveur
+        done();
+    })
+
+    it("should get token",(done)=>{
+        done();
+    })
+
+
+    afterAll(()=>{
+        //éteindre le serveur
+    })
+})
+
+describe("testing private routes", ()=>{
+    beforeAll((done)=>{
+        //démarrer une instance du serveur
+        //definir le token
+        done();
+    })
+
+
+    it("should get token",(done)=>{
+        done();
+    })
+
+
+    afterAll(()=>{
+        //éteindre le serveur
+    })
+})
