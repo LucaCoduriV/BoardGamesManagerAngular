9f5b9351a1468cb2ae8f9a06b1dbce4fb41e1077
LucaCoduriV <luca.coduri@cpnv.ch>
2020-02-13 18:08:06 +0100

    ng g c de search-result & collection

--- Frontend/angular-bgmanager/src/app/app.module.ts
+++ Frontend/angular-bgmanager/src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { HeaderComponent } from './components/header/header.component';
-import { SearchbarComponent } from './components/searchbar/searchbar.component';
-import { GamelistComponent } from './components/gamelist/gamelist.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { SearchbarComponent } from "./components/searchbar/searchbar.component";
+import { GamelistComponent } from "./components/gamelist/gamelist.component";
+import { SearchResultComponent } from "./components/search-result/search-result.component";
+import { CollectionComponent } from "./components/collection/collection.component";
 
 @NgModule({
   declarations: [
@@ -14,13 +16,12 @@ import { GamelistComponent } from './components/gamelist/gamelist.component';
     SidebarComponent,
     HeaderComponent,
     SearchbarComponent,
-    GamelistComponent
+    GamelistComponent,
+    SearchResultComponent,
+    CollectionComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
