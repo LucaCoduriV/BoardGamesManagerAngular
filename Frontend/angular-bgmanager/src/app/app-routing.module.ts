5084d0b3ab1f23116f16918544df4bbbe5262356
LucaCoduriV <luca.coduri@cpnv.ch>
2020-02-25 16:08:51 +0100

    Angular création route + lien

--- Frontend/angular-bgmanager/src/app/app-routing.module.ts
+++ Frontend/angular-bgmanager/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { SurveyComponent } from "./components/survey/survey.component";
+import { GamelistComponent } from "./components/gamelist/gamelist.component";
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { SearchResultComponent } from "./components/search-result/search-result.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "survey", component: SurveyComponent },
+  { path: "gamelist", component: GamelistComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "search-result", component: SearchResultComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
