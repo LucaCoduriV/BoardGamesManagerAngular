817d824f08bd2b87d2f94074e7236ee990cfd3c1
LucaCoduriV <luca.coduri@cpnv.ch>
2020-02-25 16:36:11 +0100

    Angular traduction sidebar

--- Frontend/angular-bgmanager/src/app/app-routing.module.ts
+++ Frontend/angular-bgmanager/src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { SurveyComponent } from "./components/survey/survey.component";
-import { GamelistComponent } from "./components/gamelist/gamelist.component";
 import { LoginComponent } from "./components/login/login.component";
 import { RegisterComponent } from "./components/register/register.component";
 import { SearchResultComponent } from "./components/search-result/search-result.component";
+import { CollectionComponent } from "./components/collection/collection.component";
 
 const routes: Routes = [
   { path: "survey", component: SurveyComponent },
-  { path: "gamelist", component: GamelistComponent },
+  { path: "collection", component: CollectionComponent },
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent },
   { path: "search-result", component: SearchResultComponent }
