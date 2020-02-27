cd9bdab4bd953dd69ad4f5d13b8d2b7a281d3a5d
Luca.CODURI <luca.coduri@cpnv.ch>
2020-02-27 16:45:49 +0100

    API ajout d'un beforeall pour les tests
    Avant chaque test, je supprime l'utilisateur de test dans la bd

--- Backend/spec/routes.spec.js
+++ Backend/spec/routes.spec.js
@@ -1,17 +1,27 @@
 const request = require("supertest");
 const app = require("../index");
+const usrMgr = require("../model/users-management");
+
+const user = {
+    username: "testUser",
+    password: "testPassword"
+};
 
 describe("Server", () => {
     describe("ROUTES TEST", () => {
+        beforeAll(done => {
+            usrMgr.deleteUser(user.username, done);
+        });
+
         it("should add a user", done => {
-            const user = {
-                username: "Bastian",
-                password: "pro"
-            };
             request(app)
                 .post("/register-user")
                 .send(user)
                 .expect(201, done);
+        });
+
+        it("should check user password", done => {
+            done();
         });
     });
 });
