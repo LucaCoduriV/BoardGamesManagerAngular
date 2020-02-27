a7896830b3f6eeea064278d23d694c170954d03d
Luca Coduri <coduri.vitor@gmail.com>
2020-02-27 21:28:55 +0100

    API ajout de test
    - test route get-game-info-API

--- Backend/spec/routes.spec.js
+++ Backend/spec/routes.spec.js
@@ -32,11 +32,17 @@ describe("route", () => {
             usrMgr.deleteUser(user.username, done);
         });
 
-        it("should return 201", done => {
+        it("should return 201 and create user", done => {
             request(app)
                 .post("/register-user")
                 .send(user)
-                .expect(201, done);
+                .expect(201)
+                .then(response => {
+                    usrMgr.selectUser(user.username, (err, result) => {
+                        expect(result.length).toEqual(1);
+                        done();
+                    });
+                });
         });
     });
 
@@ -68,7 +74,7 @@ describe("route", () => {
     });
 
     describe("/search-games-API/:name", () => {
-        it("should return defined item", done => {
+        it("should return search result", done => {
             request(app)
                 .post("/search-games-API/monopoly")
                 .expect(200)
@@ -78,7 +84,7 @@ describe("route", () => {
                 });
         });
 
-        it("should return defined item", done => {
+        it("should return no result", done => {
             request(app)
                 .post("/search-games-API/sakfhksjhf")
                 .expect(200)
@@ -88,6 +94,48 @@ describe("route", () => {
                 });
         });
     });
+
+    describe("/get-game-info-API/:id", () => {
+        it("should return search result", done => {
+            request(app)
+                .post("/get-game-info-API/2740") //aero Monopoly
+                .expect(200)
+                .then(response => {
+                    expect(response.body.item).toBeDefined();
+                    done();
+                });
+        });
+
+        it("should return no result", done => {
+            request(app)
+                .post("/get-game-info-API/999999999999")
+                .expect(200)
+                .then(response => {
+                    expect(response.body.item).toBeUndefined();
+                    done();
+                });
+        });
+    });
+
+    describe("/vote", () => {});
+
+    describe("/get-collection", () => {});
+
+    describe("/get-game-info-collection", () => {});
+
+    describe("/add-game-in-collection", () => {});
+
+    describe("/modify-game-in-collection", () => {});
+
+    describe("/delete-game-from-collection", () => {});
+
+    describe("/create-survey", () => {});
+
+    describe("/vote-while-logged", () => {});
+
+    describe("/get-sharelink-survey", () => {});
+
+    describe("/delete-user", () => {});
 
     afterAll(done => {
         usrMgr.deleteUser(user.username, done);
