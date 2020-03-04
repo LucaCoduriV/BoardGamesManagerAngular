bea884511b81bac87e201473a50f448eecfc2262
Luca Coduri <coduri.vitor@gmail.com>
2020-03-04 18:58:22 +0100

    Angular ajout d'un service pour gérer les erreus

--- Frontend/angular-bgmanager/src/app/services/errorsHandler.service.ts
+++ Frontend/angular-bgmanager/src/app/services/errorsHandler.service.ts
@@ -4,9 +4,10 @@ import { throwError } from "rxjs";
 @Injectable({
     providedIn: "root"
 })
-export class errorsHandler {
+export class ErrorsHandler {
     handleError(error) {
         let errorMessage: object;
+        console.log(error);
         if (error.error instanceof ErrorEvent) {
             // client-side error
             errorMessage = { message: error.error.message };
@@ -14,6 +15,28 @@ export class errorsHandler {
             // server-side error
             errorMessage = { code: error.status, message: error.error };
         }
+        return throwError(errorMessage);
+    }
+    handleLoginError(error) {
+        let errorMessage: object;
+        console.log(error);
+        switch (error.status) {
+            case 401:
+                errorMessage = {
+                    code: error.status,
+                    message: "Mauvais mot de passe ou utilisateur",
+                    type: "danger"
+                };
+                break;
+            default:
+                errorMessage = {
+                    code: error.status,
+                    message: error.error,
+                    type: "danger"
+                };
+                break;
+        }
+
         return throwError(errorMessage);
     }
 }
