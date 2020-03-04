5374f821aae4bfd18e5679adda55ac5e3c4f9ba0
Luca Coduri <coduri.vitor@gmail.com>
2020-03-04 18:55:13 +0100

    API correction du message d'erreur inscritption

--- Backend/controllers/users-controller.js
+++ Backend/controllers/users-controller.js
@@ -4,156 +4,156 @@ const jwt = require("jsonwebtoken");
 
 //login register
 function registerUser(req, res) {
-  let username = req.body.username;
-  let password = req.body.password;
-  usersManagement.addUser({ username: username, password: password }, (err, result) => {
-    if (err) {
-      res.status(520).send(err);
-    } else {
-      res.status(201).send("Utilisateur ajouté avec succès !");
-    }
-  });
+    let username = req.body.username;
+    let password = req.body.password;
+    usersManagement.addUser({ username: username, password: password }, (err, result) => {
+        if (err) {
+            res.status(409).send("L'utilisateur existe déjà");
+        } else {
+            res.status(201).send("Utilisateur ajouté avec succès !");
+        }
+    });
 }
 
 function login(req, res) {
-  let username = req.body.username;
-  let password = req.body.password;
+    let username = req.body.username;
+    let password = req.body.password;
 
-  usersManagement.selectUser(username, (err, result) => {
-    if (result.length == 0) {
-      //si l'utilisateur n'existe pas dans la bd
-      res.status(500).send("Non autorisé");
-    } else {
-      //checker si le hash correspond
-      bcrypt.compare(password, result[0].password, (err, hashOK) => {
-        if (hashOK) {
-          let token = jwt.sign({ idUser: result[0].idUser, superadmin: result[0].superadmin }, process.env.JWT_SECRET);
-          res.status(201).send({ token: token });
+    usersManagement.selectUser(username, (err, result) => {
+        if (result.length == 0) {
+            //si l'utilisateur n'existe pas dans la bd
+            res.status(401).send("Non autorisé");
         } else {
-          res.status(500).send("Non autorisé");
+            //checker si le hash correspond
+            bcrypt.compare(password, result[0].password, (err, hashOK) => {
+                if (hashOK) {
+                    let token = jwt.sign({ idUser: result[0].idUser, superadmin: result[0].superadmin }, process.env.JWT_SECRET);
+                    res.status(201).send({ token: token });
+                } else {
+                    res.status(401).send("Non autorisé");
+                }
+            });
         }
-      });
-    }
-  });
+    });
 }
 
 //collection
 function getCollection(req, res) {
-  let idUser = req.jwt.idUser;
+    let idUser = req.jwt.idUser;
 
-  usersManagement.getCollection(idUser, (err, result) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(200).send(result);
-    }
-  });
+    usersManagement.getCollection(idUser, (err, result) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(result);
+        }
+    });
 }
 
 function getGameInfoCollection(req, res) {
-  let idUser = req.jwt.idUser;
-  let idGame = req.params.idGames;
+    let idUser = req.jwt.idUser;
+    let idGame = req.params.idGames;
 
-  usersManagement.getGameInfoCollection(idUser, idGame, (err, result) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(200).send(result);
-    }
-  });
+    usersManagement.getGameInfoCollection(idUser, idGame, (err, result) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(result);
+        }
+    });
 }
 
 function addGameInCollection(req, res) {
-  //La liste des paramètres récupéré du formulaire
-  let idAPI = req.body.idAPI;
-  let gameName = req.body.gameName;
-  let description = req.body.description;
-  let minAge = req.body.minAge;
-  let minNbPlayer = req.body.minNbPlayer;
-  let maxNbPlayer = req.body.maxNbPlayer;
-  let minDuration = req.body.minDuration;
-  let maxDuration = req.body.maxDuration;
-  let creationDate = req.body.creationDate;
-  let idUser = req.jwt.idUser;
+    //La liste des paramètres récupéré du formulaire
+    let idAPI = req.body.idAPI;
+    let gameName = req.body.gameName;
+    let description = req.body.description;
+    let minAge = req.body.minAge;
+    let minNbPlayer = req.body.minNbPlayer;
+    let maxNbPlayer = req.body.maxNbPlayer;
+    let minDuration = req.body.minDuration;
+    let maxDuration = req.body.maxDuration;
+    let creationDate = req.body.creationDate;
+    let idUser = req.jwt.idUser;
 
-  usersManagement.addGameInCollection(
-    {
-      idAPI: idAPI,
-      gameName: gameName,
-      description: description,
-      minAge: minAge,
-      minNbPlayer: minNbPlayer,
-      maxNbPlayer: maxNbPlayer,
-      minDuration: minDuration,
-      maxDuration: maxDuration,
-      creationDate: creationDate,
-      idUser: idUser
-    },
-    (err, result) => {
-      if (err) {
-        res.status(520).send(err);
-      } else {
-        res.status(201).send("Jeu ajouté avec succès !");
-      }
-    }
-  );
+    usersManagement.addGameInCollection(
+        {
+            idAPI: idAPI,
+            gameName: gameName,
+            description: description,
+            minAge: minAge,
+            minNbPlayer: minNbPlayer,
+            maxNbPlayer: maxNbPlayer,
+            minDuration: minDuration,
+            maxDuration: maxDuration,
+            creationDate: creationDate,
+            idUser: idUser
+        },
+        (err, result) => {
+            if (err) {
+                res.status(520).send(err);
+            } else {
+                res.status(201).send("Jeu ajouté avec succès !");
+            }
+        }
+    );
 }
 
 function modifyGameInCollection(req, res) {
-  let idGame = req.params.idGame;
-  let idUser = req.jwt.idUser;
-  let gameName = req.body.gameName;
-  let description = req.body.description;
-  let minAge = req.body.minAge;
-  let minNbPlayer = req.body.minNbPlayer;
-  let maxNbPlayer = req.body.maxNbPlayer;
-  let minDuration = req.body.minDuration;
-  let maxDuration = req.body.maxDuration;
+    let idGame = req.params.idGame;
+    let idUser = req.jwt.idUser;
+    let gameName = req.body.gameName;
+    let description = req.body.description;
+    let minAge = req.body.minAge;
+    let minNbPlayer = req.body.minNbPlayer;
+    let maxNbPlayer = req.body.maxNbPlayer;
+    let minDuration = req.body.minDuration;
+    let maxDuration = req.body.maxDuration;
 
-  usersManagement.modifyGameInCollection(
-    {
-      idGame: idGame,
-      idUser: idUser,
-      gameName: gameName,
-      description: description,
-      minAge: minAge,
-      minNbPlayer: minNbPlayer,
-      maxNbPlayer: maxNbPlayer,
-      minDuration: minDuration,
-      maxDuration: maxDuration
-    },
-    (err, result) => {
-      if (result.affectedRows == 0) {
-        res.status(500).send("Impossible de modifier le jeu");
-      } else {
-        res.status(201).send("Jeu modifié avec succès !");
-      }
-    }
-  );
+    usersManagement.modifyGameInCollection(
+        {
+            idGame: idGame,
+            idUser: idUser,
+            gameName: gameName,
+            description: description,
+            minAge: minAge,
+            minNbPlayer: minNbPlayer,
+            maxNbPlayer: maxNbPlayer,
+            minDuration: minDuration,
+            maxDuration: maxDuration
+        },
+        (err, result) => {
+            if (result.affectedRows == 0) {
+                res.status(500).send("Impossible de modifier le jeu");
+            } else {
+                res.status(201).send("Jeu modifié avec succès !");
+            }
+        }
+    );
 }
 
 function deleteGameFromCollection(req, res) {
-  let idGame = req.params.idGame;
-  let idUser = req.jwt.idUser;
+    let idGame = req.params.idGame;
+    let idUser = req.jwt.idUser;
 
-  usersManagement.deleteGameFromCollection(idGame, idUser, (err, result) => {
-    if (result.affectedRows == 0) {
-      res.status(400).send("Impossible de supprimer le jeu");
-    } else {
-      res.status(200).send("Jeu supprimé avec succès !");
-    }
-  });
+    usersManagement.deleteGameFromCollection(idGame, idUser, (err, result) => {
+        if (result.affectedRows == 0) {
+            res.status(400).send("Impossible de supprimer le jeu");
+        } else {
+            res.status(200).send("Jeu supprimé avec succès !");
+        }
+    });
 }
 
 //admin
 function deleteUser(req, res) {
-  usersManagement.deleteUser(req.params.idUser, (err, result) => {
-    if (result.affectedRows == 0) {
-      res.status(404).send("user do not exist!");
-    } else {
-      res.status(200).send("user deleted succesfully!");
-    }
-  });
+    usersManagement.deleteUser(req.params.idUser, (err, result) => {
+        if (result.affectedRows == 0) {
+            res.status(404).send("user do not exist!");
+        } else {
+            res.status(200).send("user deleted succesfully!");
+        }
+    });
 }
 
 exports.registerUser = registerUser;
--- Backend/model/users-management.js
+++ Backend/model/users-management.js
@@ -4,105 +4,101 @@ const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
 function selectAll(callback) {
-  DB.pool.query("SELECT * FROM users;", (err, result) => {
-    callback(err, result);
-  });
+    DB.pool.query("SELECT * FROM users;", (err, result) => {
+        callback(err, result);
+    });
 }
 
 function selectUser(username, callback) {
-  DB.pool.query(`SELECT * FROM users WHERE login = '${username}';`, (err, result) => {
-    callback(err, result);
-  });
+    DB.pool.query(`SELECT * FROM users WHERE login = '${username}';`, (err, result) => {
+        callback(err, result);
+    });
 }
 
 function addUser(user, callback) {
-  if (user.id == undefined) user.id = null;
+    if (user.id == undefined) user.id = null;
 
-  function hashPassword() {
-    bcrypt.hash(user.password, saltRounds, (err, hash) => {
-      if (err) {
-        callback(err);
-      } else {
-        DB.pool.query(`INSERT INTO users(idUser, login, password) values(${user.id}, '${user.username}', '${hash}');`, (err, result) => {
-          callback(err, result);
+    function hashPassword() {
+        bcrypt.hash(user.password, saltRounds, (err, hash) => {
+            if (err) {
+                callback(err);
+            } else {
+                DB.pool.query(`INSERT INTO users(idUser, login, password) values(${user.id}, '${user.username}', '${hash}');`, (err, result) => {
+                    callback(err, result);
+                });
+            }
         });
-      }
-    });
-  }
-
-  selectUser(user.username, (err, result) => {
-    if (result.length > 0) {
-      err = {
-        code: 1,
-        message: "Nom d'utilisateur déjà pris"
-      };
-      callback(err);
-    } else {
-      hashPassword();
     }
-  });
+    //Check si l'utilisateur existe déjà dans la bd
+    selectUser(user.username, (err, result) => {
+        if (result.length > 0) {
+            callback((err = true)); //oui il y a une erreur
+        } else {
+            hashPassword();
+        }
+    });
 }
 
 function deleteUser(idUser, callback) {
-  DB.pool.query(`DELETE FROM users WHERE (idUser = '${idUser}');`, (err, result) => {
-    callback(err, result);
-  });
+    DB.pool.query(`DELETE FROM users WHERE (idUser = '${idUser}');`, (err, result) => {
+        callback(err, result);
+    });
 }
 
 function getCollection(idUser, callback) {
-  DB.pool.query(
-    `SELECT games.name, games.description, games.minAge, games.minNbPlayer, games.maxNbPlayer, games.minDuration, games.maxDuration, games.creationDate 
+    DB.pool.query(
+        `SELECT games.name, games.description, games.minAge, games.minNbPlayer, games.maxNbPlayer, games.minDuration, games.maxDuration, games.creationDate 
     FROM games 
     WHERE games.idUser = ${idUser};`,
-    (err, result) => {
-      callback(err, result);
-    }
-  );
+        (err, result) => {
+            callback(err, result);
+        }
+    );
 }
 
 function getGameInfoCollection(idUser, idGame, callback) {
-  DB.pool.query(
-    `SELECT games.name, games.description, games.minAge, games.minNbPlayer, games.maxNbPlayer, games.minDuration, games.maxDuration, games.creationDate 
+    DB.pool.query(
+        `SELECT games.name, games.description, games.minAge, games.minNbPlayer, games.maxNbPlayer, games.minDuration, games.maxDuration, games.creationDate 
   FROM games 
   WHERE games.idUser = ${idUser} 
   AND games.idGame = ${idGame};`,
-    (err, result) => {
-      callback(err, result);
-    }
-  );
+        (err, result) => {
+            callback(err, result);
+        }
+    );
 }
 
 function addGameInCollection(game, callback) {
-  if (game.idAPI == undefined) game.idAPI = null;
-  if (game.gameName == undefined) game.gameName = null;
-  if (game.description == undefined) game.description = null;
-  if (game.minAge == undefined) game.minAge = null;
-  if (game.minNbPlayer == undefined) game.minNbPlayer = null;
-  if (game.maxNbPlayer == undefined) game.maxNbPlayer = null;
-  if (game.minDuration == undefined) game.minDuration = null;
-  if (game.maxDuration == undefined) game.maxDuration = null;
-  if (game.creationDate == undefined) game.creationDate = null;
-  else game.creationDate = "'" + game.creationDate + "'";
+    if (game.idAPI == undefined) game.idAPI = null;
+    if (game.gameName == undefined) game.gameName = null;
+    if (game.description == undefined) game.description = null;
+    if (game.minAge == undefined) game.minAge = null;
+    if (game.minNbPlayer == undefined) game.minNbPlayer = null;
+    if (game.maxNbPlayer == undefined) game.maxNbPlayer = null;
+    if (game.minDuration == undefined) game.minDuration = null;
+    if (game.maxDuration == undefined) game.maxDuration = null;
+    if (game.creationDate == undefined) game.creationDate = null;
+    else game.creationDate = "'" + game.creationDate + "'";
 
-  DB.pool.query(
-    `INSERT INTO games VALUES(null, ${game.idAPI}, '${game.gameName}', '${game.description}', ${game.minAge}, ${game.minNbPlayer}, ${game.maxNbPlayer}, ${game.minDuration}, ${game.maxDuration}, ${game.creationDate}, ${game.idUser});`,
-    (err, result) => {
-      callback(err, result);
-    }
-  );
+    DB.pool.query(
+        `INSERT INTO games VALUES(null, ${game.idAPI}, '${game.gameName}', '${game.description}', ${game.minAge}, ${game.minNbPlayer}, ${game.maxNbPlayer}, ${game.minDuration}, ${game.maxDuration}, ${game.creationDate}, ${game.idUser});`,
+        (err, result) => {
+            callback(err, result);
+        }
+    );
 }
 
 function modifyGameInCollection(game, callback) {
-  if (game.gameName == undefined) game.gameName = null;
-  if (game.description == undefined) game.description = null;
-  if (game.minAge == undefined) game.minAge = null;
-  if (game.minNbPlayer == undefined) game.minNbPlayer = null;
-  if (game.maxNbPlayer == undefined) game.maxNbPlayer = null;
-  if (game.minDuration == undefined) game.minDuration = null;
-  if (game.maxDuration == undefined) game.maxDuration = null;
+    if (game.gameName == undefined) game.gameName = null;
+    if (game.description == undefined) game.description = null;
+    if (game.minAge == undefined) game.minAge = null;
+    if (game.minNbPlayer == undefined) game.minNbPlayer = null;
+    if (game.maxNbPlayer == undefined) game.maxNbPlayer = null;
+    if (game.minDuration == undefined) game.minDuration = null;
+    if (game.maxDuration == undefined) game.maxDuration = null;
 
-  DB.pool.query(
-    `UPDATE games 
+    DB.pool.query(
+        `UPDATE games 
     SET 
     name = '${game.gameName}',
     description = '${game.description}',
@@ -113,16 +109,16 @@ function modifyGameInCollection(game, callback) {
     maxDuration = ${game.maxDuration}
     WHERE idGame = ${game.idGame}
     AND idUser = ${game.idUser};`,
-    (err, result) => {
-      callback(err, result);
-    }
-  );
+        (err, result) => {
+            callback(err, result);
+        }
+    );
 }
 
 function deleteGameFromCollection(idGame, idUser, callback) {
-  DB.pool.query(`DELETE FROM games WHERE idGame = ${idGame} and idUser = ${idUser}`, (err, result) => {
-    callback(err, result);
-  });
+    DB.pool.query(`DELETE FROM games WHERE idGame = ${idGame} and idUser = ${idUser}`, (err, result) => {
+        callback(err, result);
+    });
 }
 
 exports.selectAll = selectAll;
