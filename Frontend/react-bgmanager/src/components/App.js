3e0b085353cb40b10944be5833b8a2d800c75ff4
bchollet <babacouda@gmail.com>
2020-03-09 11:16:07 +0100

    React Initialisation des composants HomePage et NavBar

--- Frontend/react-bgmanager/src/components/App.js
+++ Frontend/react-bgmanager/src/components/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomePage from "./HomePage";
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Router>
-          <Route exact path="/" />
+          <Route exact path="/" component={HomePage} />
         </Router>
       </div>
     );
---
+++ Frontend/react-bgmanager/src/components/HeaderComponent/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default class NavBar extends React.Component {
+  render() {
+    return (
+      <header>
+        <ul id="headerButtons">
+          <li className="navButton">
+            <Link to="">Home</Link>
+          </li>
+        </ul>
+      </header>
+    );
+  }
+}
