2620062f41f254eadb900be71ef4313ecb813958
bchollet <babacouda@gmail.com>
2020-03-12 16:54:13 +0100

    React Création de LoginForm. Non-fontionnel

--- Frontend/react-bgmanager/src/components/Forms/LoginForm.js
+++ Frontend/react-bgmanager/src/components/Forms/LoginForm.js
@@ -1,12 +1,50 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import "./loginForm.css";
 
 export default class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: undefined,
+      password: undefined
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.username);
+    console.log(this.state.password);
+
+    fetch("http://localhost:8081/login", {
+      method: "POST",
+      body: this.state
+    }).then(value => {
+      console.log(value);
+    });
+  }
+
+  handleChange(event) {
+    console.log(event.target.value);
+    this.setState({
+      username: event.target.value
+    });
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h2>Login</h2>
-        <input type="text"></input>
+        <div id="form-input-text">
+          <input name="username" value={this.state.username} type="text" placeholder="Nom d'utilisateur" onChange={this.handleChange} />
+          <br />
+          <input name="password" value={this.state.password} type="password" placeholder="Mot de passe" onChange={this.handleChange} />
+        </div>
+        <div>
+          <button type="submit">Se connecter</button>
+          <button type="reset">Vider les champs</button>
+        </div>
       </form>
     );
   }
