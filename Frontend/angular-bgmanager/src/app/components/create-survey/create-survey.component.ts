5e870ad897de8eed4a6c3dbdf0fc6fd40ceb8c96
LucaCoduriV <luca.coduri@cpnv.ch>
2020-03-13 15:00:24 +0100

    Angular les jeux de la collection sont ajouté à la liste

--- Frontend/angular-bgmanager/src/app/components/create-survey/create-survey.component.ts
+++ Frontend/angular-bgmanager/src/app/components/create-survey/create-survey.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl } from "@angular/forms";
+import { GetService } from "src/app/services/http-requests/get.service";
+import { Observable } from "rxjs";
 
 @Component({
     selector: "app-create-survey",
@@ -7,12 +9,40 @@ import { FormGroup, FormControl } from "@angular/forms";
     styleUrls: ["./create-survey.component.scss"]
 })
 export class CreateSurveyComponent implements OnInit {
+    gamelist$: Observable<any>;
+    selectedCollectionGame: number;
+    listOfSelectedGames: Array<any> = [];
+    usersCollection: Array<any>;
+
     createSurveyForm = new FormGroup({
         username: new FormControl(""),
         password: new FormControl("")
     });
 
-    constructor() {}
+    constructor(private getService: GetService) {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.gamelist$ = this.getService.getUserCollection(
+            +localStorage.getItem("idUser")
+        );
+        this.gamelist$.subscribe(value => {
+            this.usersCollection = value;
+        });
+    }
+    onSelectChange(value: string) {
+        this.selectedCollectionGame = +value;
+    }
+
+    addCollectionGameInList() {
+        this.listOfSelectedGames.push(
+            this.usersCollection.find(
+                value => this.selectedCollectionGame == value.idGame
+            )
+        );
+    }
+    removeGame(id: number) {
+        this.listOfSelectedGames = this.listOfSelectedGames.filter(element => {
+            return element.idGame != +id;
+        });
+    }
 }
