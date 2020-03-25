33f624bfa2ee7140ebda859e54457a5621938cd0
Bastian <bastian.chollet@cpnv.ch>
2020-03-25 15:38:36 +0100

    React Définition d'un utilisateur loggé par vérification de la présence du token dans LocalStorage

--- Frontend/react-bgmanager/src/components/App.js
+++ Frontend/react-bgmanager/src/components/App.js
@@ -11,7 +11,7 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLogged: false
+      isLogged: localStorage.getItem('token') != null
     };
   }
 
