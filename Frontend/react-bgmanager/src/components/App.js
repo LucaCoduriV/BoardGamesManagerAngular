341bef0e510145c56e5a800b27809072b4d42d5c
Bastian <bastian.chollet@cpnv.ch>
2020-04-15 15:05:35 +0200

    React Création de la HeaderBar

--- Frontend/react-bgmanager/src/components/App.js
+++ Frontend/react-bgmanager/src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import HeaderBar from "./HeaderBar"
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <HeaderBar />
         <Router>
           <Route exact path="/" />
         </Router>
