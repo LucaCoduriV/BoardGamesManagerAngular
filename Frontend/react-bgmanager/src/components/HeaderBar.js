64a469b53409b7996fc34b9f176009634413b1ee
Bastian <bastian.chollet@cpnv.ch>
2020-04-15 17:06:32 +0200

    React Update design HeaderBar

--- Frontend/react-bgmanager/src/components/HeaderBar.js
+++ Frontend/react-bgmanager/src/components/HeaderBar.js
@@ -1,22 +1,50 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { fade, makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
+      width: '100%',
   },
   title: {
-    flexGrow: 1,
+      minWidth: '211px',
+      marginRight: '30px',
   },
+  search: {
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0.5, 2),
+    paddingTop: '6px',
+    position: 'absolute',
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '400px'
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingTop: '10px',
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+  },
+  buttonWrapper : {
+      width: '100%',
+  },
+  button : {
+      float: 'right',
+  }
 }));
 
 export default function HeaderBar() {
@@ -26,13 +54,25 @@ export default function HeaderBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" className={classes.title}>
-            News
+            Board Games Manager
           </Typography>
-          <Button color="inherit">Login</Button>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Recherche"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+            />
+          </div>
+          <div className={classes.buttonWrapper}>
+            <Button color="inherit" className={classes.button}>Inscription</Button>
+            <Button color="inherit" className={classes.button}>Connexion</Button>
+          </div>
         </Toolbar>
       </AppBar>
     </div>
