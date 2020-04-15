1d9e45c0e07db4c3d00cb53ccbf982f45457b5cb
Bastian <bastian.chollet@cpnv.ch>
2020-04-15 15:11:35 +0200

    React Update écriture de App.js

--- Frontend/react-bgmanager/src/components/App.js
+++ Frontend/react-bgmanager/src/components/App.js
@@ -2,15 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import HeaderBar from "./HeaderBar"
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <HeaderBar />
-        <Router>
-          <Route exact path="/" />
-        </Router>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <HeaderBar />
+      <Router>
+        <Route exact path="/" />
+      </Router>
+    </div>
+  );
 }
