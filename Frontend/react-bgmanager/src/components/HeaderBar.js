1cbd5f981ab30de956b7591ea4b4620319c057c9
bchollet <babacouda@gmail.com>
2020-05-08 13:55:10 +0200

    React Update HeaderBar

--- Frontend/react-bgmanager/src/components/HeaderBar.js
+++ Frontend/react-bgmanager/src/components/HeaderBar.js
@@ -1,80 +1,114 @@
 import React from 'react';
-import { fade, makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import InputBase from '@material-ui/core/InputBase';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-      width: '100%',
-  },
-  title: {
-      minWidth: '211px',
-      marginRight: '30px',
-  },
-  search: {
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-  },
-  searchIcon: {
-    padding: theme.spacing(0.5, 2),
-    paddingTop: '6px',
-    position: 'absolute',
-  },
-  inputRoot: {
-    color: 'inherit',
-    width: '400px'
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingTop: '10px',
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-  },
-  buttonWrapper : {
-      width: '100%',
-  },
-  button : {
-      float: 'right',
-  }
+	root: {
+		flexGrow: 1,
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+	},
+	title: {
+		display: 'none',
+		[theme.breakpoints.up('sm')]: {
+			display: 'block',
+			marginRight: '10px',
+		},
+	},
+	search: {
+		position: 'relative',
+		borderRadius: theme.shape.borderRadius,
+		backgroundColor: fade(theme.palette.common.white, 0.15),
+		'&:hover': {
+			backgroundColor: fade(theme.palette.common.white, 0.25),
+		},
+		marginLeft: 0,
+		width: '100%',
+		[theme.breakpoints.up('sm')]: {
+			marginLeft: theme.spacing(1),
+			width: 'auto',
+		},
+	},
+	searchIcon: {
+		padding: theme.spacing(0, 2),
+		height: '100%',
+		position: 'absolute',
+		pointerEvents: 'none',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	inputRoot: {
+		color: 'inherit',
+	},
+	inputInput: {
+		padding: theme.spacing(1, 1, 1, 0),
+		// vertical padding + font size from searchIcon
+		paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+		transition: theme.transitions.create('width'),
+		width: '100%',
+		[theme.breakpoints.up('sm')]: {
+			width: '20ch',
+			'&:focus': {
+				width: '28ch',
+			},
+		},
+	},
+	button: {
+		color: 'white',
+	},
 }));
 
-export default function HeaderBar() {
-  const classes = useStyles();
+export default function SearchAppBar() {
+	const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            Board Games Manager
-          </Typography>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              placeholder="Recherche"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-            />
-          </div>
-          <div className={classes.buttonWrapper}>
-            <Button color="inherit" className={classes.button}>Inscription</Button>
-            <Button color="inherit" className={classes.button}>Connexion</Button>
-          </div>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+	return (
+		<div className={classes.root}>
+			<AppBar position='static'>
+				<Toolbar>
+					<IconButton
+						edge='start'
+						className={classes.menuButton}
+						color='inherit'
+						aria-label='open drawer'>
+						<MenuIcon />
+					</IconButton>
+					<Typography className={classes.title} variant='h6' noWrap>
+						Board Games Manager
+					</Typography>
+					<div className={classes.search}>
+						<div className={classes.searchIcon}>
+							<SearchIcon />
+						</div>
+						<InputBase
+							placeholder='Recherche...'
+							classes={{
+								root: classes.inputRoot,
+								input: classes.inputInput,
+							}}
+							inputProps={{ 'aria-label': 'search' }}
+						/>
+					</div>
+					<div className={classes.root} />
+					<ButtonGroup
+						className={classes.button}
+						variant='text'
+						aria-label='text button group'
+						size='large'>
+						<Button className={classes.button}>Connexion</Button>
+						<Button className={classes.button}>Inscription</Button>
+					</ButtonGroup>
+				</Toolbar>
+			</AppBar>
+		</div>
+	);
 }
