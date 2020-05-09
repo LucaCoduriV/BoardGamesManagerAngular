a52c13a961d765ce63a0a4d63512fd680c28e97f
bchollet <babacouda@gmail.com>
2020-05-09 20:16:32 +0200

    React Update menu latéral (responsive)

--- Frontend/react-bgmanager/src/components/HeaderBar.js
+++ Frontend/react-bgmanager/src/components/HeaderBar.js
@@ -6,7 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
-import { fade, makeStyles } from '@material-ui/core/styles';
+import { fade, makeStyles, useTheme } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
@@ -18,8 +18,10 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import List from '@material-ui/core/List';
 import Drawer from '@material-ui/core/Drawer';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import StarIcon from '@material-ui/icons/Star';
+import DoneIcon from '@material-ui/icons/Done';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import Hidden from '@material-ui/core/Hidden';
 
 const drawerWidth = 240; //Taille du menu tiroir gauche
 
@@ -111,12 +113,22 @@ const useStyles = makeStyles((theme) => ({
 		color: 'white',
 	},
 	appBar: {
-		width: `calc(100% - ${drawerWidth}px)`,
-		marginLeft: drawerWidth,
+		[theme.breakpoints.up('sm')]: {
+			width: `calc(100% - ${drawerWidth}px)`,
+			marginLeft: drawerWidth,
+		},
+		// width: '100%',
+		// marginLeft: 0,
 	},
 	drawer: {
-		width: drawerWidth,
-		flexShrink: 0,
+		//display: 'none',
+		[theme.breakpoints.up('sm')]: {
+			//display: 'block',
+			width: drawerWidth,
+			flexShrink: 0,
+		},
+		// width: 0,
+		// flexShrink: 0,
 	},
 	drawerPaper: {
 		width: drawerWidth,
@@ -130,8 +142,9 @@ const useStyles = makeStyles((theme) => ({
 	},
 }));
 
-export default function SearchAppBar() {
+export default function SearchAppBar(props) {
 	const [anchorEl, setAnchorEl] = React.useState(null); //Hook d'état gérant le menu de compte sur petit écran. Masqué par défaut
+	const [mobileOpen, setMobileOpen] = React.useState(false);
 
 	const handleClick = (event) => {
 		setAnchorEl(event.currentTarget); //currentTarget = Menu qui s'affiche à l'endroit où l'évènement se produit
@@ -142,7 +155,44 @@ export default function SearchAppBar() {
 		setAnchorEl(null); //fermeture du menu de compte sur petit écran
 	};
 
+	const handleDrawerToggle = () => {
+		setMobileOpen(!mobileOpen);
+	};
+
 	const classes = useStyles(); //Permet la gestion des style CSS
+	const theme = useTheme();
+	const { window } = props;
+
+	const container =
+		window !== undefined ? () => window().document.body : undefined;
+
+	const drawer = (
+		<div>
+			<div className={classes.toolbar} />
+			<Divider />
+			<List>
+				{['Ma collection', 'Mes sondages'].map((text, index) => (
+					<ListItem button key={text}>
+						<ListItemIcon>
+							{index % 2 === 0 ? <StarIcon /> : <DoneIcon />}
+						</ListItemIcon>
+						<ListItemText primary={text} />
+					</ListItem>
+				))}
+			</List>
+			<Divider />
+			<List>
+				{['Administration'].map((text, index) => (
+					<ListItem button key={text}>
+						<ListItemIcon>
+							<SupervisorAccountIcon />
+						</ListItemIcon>
+						<ListItemText primary={text} />
+					</ListItem>
+				))}
+			</List>
+		</div>
+	);
 
 	return (
 		<div className={classes.root}>
@@ -153,7 +203,8 @@ export default function SearchAppBar() {
 						edge='start'
 						className={classes.menuButton}
 						color='inherit'
-						aria-label='open drawer'>
+						aria-label='open drawer'
+						onClick={handleDrawerToggle}>
 						<MenuIcon className={classes.menuIcon} />
 					</IconButton>
 
@@ -207,45 +258,36 @@ export default function SearchAppBar() {
 			</AppBar>
 
 			{/* MENU LATERAL GAUCHE */}
-			<Drawer
-				className={classes.drawer}
-				variant='permanent'
-				classes={{
-					paper: classes.drawerPaper,
-				}}
-				anchor='left'>
-				{/* <div className={classes.toolbar} /> */}
 
-				<List>
-					{['Bonjour bel inconnu !'].map((text, index) => (
-						<ListItem>
-							<ListItemText primary={text} />
-						</ListItem>
-					))}
-				</List>
-				<Divider />
-				<List>
-					{['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-						<ListItem button key={text}>
-							<ListItemIcon>
-								{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-							</ListItemIcon>
-							<ListItemText primary={text} />
-						</ListItem>
-					))}
-				</List>
-				<Divider />
-				<List>
-					{['All mail', 'Trash', 'Spam'].map((text, index) => (
-						<ListItem button key={text}>
-							<ListItemIcon>
-								{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-							</ListItemIcon>
-							<ListItemText primary={text} />
-						</ListItem>
-					))}
-				</List>
-			</Drawer>
+			<nav className={classes.drawer} aria-label='mailbox folders'>
+				{/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+				<Hidden smUp implementation='css'>
+					<Drawer
+						container={container}
+						variant='temporary'
+						anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+						open={mobileOpen}
+						onClose={handleDrawerToggle}
+						classes={{
+							paper: classes.drawerPaper,
+						}}
+						ModalProps={{
+							keepMounted: true, // Better open performance on mobile.
+						}}>
+						{drawer}
+					</Drawer>
+				</Hidden>
+				<Hidden xsDown implementation='css'>
+					<Drawer
+						classes={{
+							paper: classes.drawerPaper,
+						}}
+						variant='permanent'
+						open>
+						{drawer}
+					</Drawer>
+				</Hidden>
+			</nav>
 		</div>
 	);
 }
