33d5d328f5612c7ae50ee82dcb0d9240d1823ed8
bchollet <babacouda@gmail.com>
2020-05-11 11:09:32 +0200

    React Update du Login form

--- Frontend/react-bgmanager/src/components/Forms/Login.js
+++ Frontend/react-bgmanager/src/components/Forms/Login.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
@@ -13,20 +10,13 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
-function Copyright() {
-	return (
-		<Typography variant='body2' color='textSecondary' align='center'>
-			{'Copyright © '}
-			<Link color='inherit' href='https://material-ui.com/'>
-				Your Website
-			</Link>{' '}
-			{new Date().getFullYear()}
-			{'.'}
-		</Typography>
-	);
-}
-
 const useStyles = makeStyles((theme) => ({
+	contentWrapper: {
+		marginTop: 100,
+		[theme.breakpoints.up('sm')]: {
+			marginLeft: 240,
+		},
+	},
 	paper: {
 		marginTop: theme.spacing(8),
 		display: 'flex',
@@ -35,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
 	},
 	avatar: {
 		margin: theme.spacing(1),
-		backgroundColor: theme.palette.secondary.main,
+		backgroundColor: theme.palette.primary.main,
 	},
 	form: {
 		width: '100%', // Fix IE 11 issue.
@@ -50,67 +40,61 @@ export default function SignIn() {
 	const classes = useStyles();
 
 	return (
-		<Container component='main' maxWidth='xs'>
-			<CssBaseline />
-			<div className={classes.paper}>
-				<Avatar className={classes.avatar}>
-					<LockOutlinedIcon />
-				</Avatar>
-				<Typography component='h1' variant='h5'>
-					Sign in
-				</Typography>
-				<form className={classes.form} noValidate>
-					<TextField
-						variant='outlined'
-						margin='normal'
-						required
-						fullWidth
-						id='email'
-						label='Email Address'
-						name='email'
-						autoComplete='email'
-						autoFocus
-					/>
-					<TextField
-						variant='outlined'
-						margin='normal'
-						required
-						fullWidth
-						name='password'
-						label='Password'
-						type='password'
-						id='password'
-						autoComplete='current-password'
-					/>
-					<FormControlLabel
-						control={<Checkbox value='remember' color='primary' />}
-						label='Remember me'
-					/>
-					<Button
-						type='submit'
-						fullWidth
-						variant='contained'
-						color='primary'
-						className={classes.submit}>
-						Sign In
-					</Button>
-					<Grid container>
-						<Grid item xs>
-							<Link href='#' variant='body2'>
-								Forgot password?
-							</Link>
+		<div className={classes.contentWrapper}>
+			<Container component='main' maxWidth='xs'>
+				<div className={classes.paper}>
+					<Avatar className={classes.avatar}>
+						<LockOutlinedIcon />
+					</Avatar>
+					<Typography component='h1' variant='h5'>
+						Connexion
+					</Typography>
+					<form className={classes.form} noValidate>
+						<TextField
+							variant='outlined'
+							margin='normal'
+							required
+							fullWidth
+							id='email'
+							label="Nom d'utilisateur"
+							name='email'
+							autoComplete='email'
+							autoFocus
+						/>
+						<TextField
+							variant='outlined'
+							margin='normal'
+							required
+							fullWidth
+							name='password'
+							label='Mot de passe'
+							type='password'
+							id='password'
+							autoComplete='current-password'
+						/>
+						<Button
+							type='submit'
+							fullWidth
+							variant='contained'
+							color='primary'
+							className={classes.submit}>
+							Se connecter
+						</Button>
+						<Grid container>
+							<Grid item xs>
+								<Link href='#' variant='body2'>
+									Mot de passe oublié ?
+								</Link>
+							</Grid>
+							<Grid item>
+								<Link href='#' variant='body2'>
+									{'Pas de compte ? Inscrivez-vous'}
+								</Link>
+							</Grid>
 						</Grid>
-						<Grid item>
-							<Link href='#' variant='body2'>
-								{"Don't have an account? Sign Up"}
-							</Link>
-						</Grid>
-					</Grid>
-				</form>
-			</div>
-			<Box mt={8}>
-				<Copyright />
-			</Box>
-		</Container>
+					</form>
+				</div>
+			</Container>
+		</div>
 	);
 }
