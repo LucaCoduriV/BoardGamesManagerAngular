ac607358532a30bdccecde86ef765ae94c77035a
bchollet <babacouda@gmail.com>
2020-05-13 14:08:09 +0200

    React Ajout de Redux et des actions

---
+++ Frontend/react-bgmanager/src/actions/actions.js
@@ -0,0 +1,26 @@
+//Actions utilisateurs
+const REGISTER = 'REGISTER';
+const LOGIN = 'LOGIN';
+
+//Actions collection
+const ADD_GAME = 'ADD_GAME';
+const DELETE_GAME = 'DELETE_GAME';
+const MODIFY_GAME = 'MODIFY_GAME';
+const SHOW_COLLECTION = 'SHOW_COLLECTION';
+const SHOW_GAME_COLLECTION = 'SHOW_GAME_COLLECTION';
+
+//Actions rechercher
+const GET_ALL_GAMES = 'GET_ALL_GAMES';
+const GET_GAME = 'GET_GAME';
+
+//Actions survey
+const ADD_SURVEY = 'ADD_SURVEY';
+const DELETE_SURVEY = 'DELETE_SURVEY';
+const GET_ALL_SURVEY = 'GET_ALL_SURVEYS';
+const GET_SURVEY = 'GET_SURVEY';
+const GET_CANDIDATES = 'GET_CANDIDATES';
+const GET_SHARE_CODE = 'GET_SHARE_CODE';
+
+//Actions administration
+const GET_ALL_USERS = 'GET_ALL_USERS';
+const DELETE_USER = 'DELETE_USER';
